261827b139c3272edcc4e289e26d2ff3df99684d
java2019 <java_2019@mail.ru>
2017-06-27 14:21:26 +0300

    Initial commit

---
+++ app/src/main/java/com/samples/network/wifimanagement/WiFiChangeActivity.java
@@ -0,0 +1,13 @@
+package com.samples.network.wifimanagement;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WiFiChangeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wi_fi_change);
+    }
+}
