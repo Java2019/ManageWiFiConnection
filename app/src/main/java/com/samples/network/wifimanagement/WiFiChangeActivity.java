3bfd0016fd5b9a2fdede266b2972d6791cad232e
java2019 <java_2019@mail.ru>
2017-06-27 14:29:05 +0300

    test1

--- app/src/main/java/com/samples/network/wifimanagement/WiFiChangeActivity.java
+++ app/src/main/java/com/samples/network/wifimanagement/WiFiChangeActivity.java
@@ -1,13 +1,43 @@
 package com.samples.network.wifimanagement;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class WiFiChangeActivity extends AppCompatActivity {
+public class WiFiChangeActivity extends Activity
+        implements View.OnClickListener{
+
+    private TextView textView;
+    private BroadcastReceiver receiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wi_fi_change);
+
+        textView = (TextView)findViewById(R.id.text);
+
+        receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+
+            }
+        };
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.bEnable:
+
+                break;
+            case R.id.bDisable:
+
+                break;
+        }
     }
 }
